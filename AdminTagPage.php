0a0638852dd4d00aa1a71cfbcd698814683605e3
Your GitHub FogKob <warunyou1038@gmail.com>
2025-03-01 01:02:50 +0700

    Update AdminTagPage.php with new changes

--- AdminTagPage.php
+++ AdminTagPage.php
@@ -64,7 +64,7 @@ session_start();
                                     <hr>
                                     <a href="#" class="sub-menu-link">
                                         <img src="images/profile.png">
-                                        <p>Edit Profile12</p>
+                                        <p>Edit Profile</p>
                                         <span></span>
                                     </a>
 
